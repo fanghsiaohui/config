6180d6176cc89772a6082acc938bc25c399f3a6f
fang <fang@arch>
2018-09-29 09:22:26 +0800

    init config

---
+++ julia-startup.jl
@@ -0,0 +1,6 @@
+# This file should contain site-specific commands to be executed on Julia startup;
+# Users may store their own personal commands in `~/.julia/config/startup.jl`.
+# add by me and this folder
+# ENV["PLOTS_DEFAULT_BACKEND"] = "PyPlot"
+ENV["PYTHON"] = "python"
+ENV["JUPYTER"] = "jupyter"
