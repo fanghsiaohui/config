0d2f252b1f491378ed11b414945040911667eb00
fang <fang@phone>
2020-10-27 09:48:55 +0800

    rm rub files

--- julia-startup.jl
+++
@@ -1,6 +0,0 @@
-# This file should contain site-specific commands to be executed on Julia startup;
-# Users may store their own personal commands in `~/.julia/config/startup.jl`.
-# add by me and this folder
-# ENV["PLOTS_DEFAULT_BACKEND"] = "PyPlot"
-ENV["PYTHON"] = "python"
-ENV["JUPYTER"] = "jupyter"
